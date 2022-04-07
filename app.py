577a2cdb45e5f78ebc62007bd94b5b2a5d42dc01
dileepkumar001 <hosteldileep01@gmail.com>
2022-04-07 06:39:26 +0530

    'adding_new_file'

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
-print("hello python")
-print("hello dileep")
+whoryou=input("who are you?\n")
+interest=input("out of 10 give the rate in number yourself how you are interested in this project \n")
+#interest=int(interest)
+try:
+    if interest.isdigit():
+        interest=int(interest)
+except:
+    print("run again and enter integer value out of 10 in second time")
+
+def check_interest(interest):
+    try:
+        
+        if interest>7:
+            return 1
+        else:
+            return 0
+    except:
+        print("type")
+
+check_y_n=check_interest(interest)
+try:
+    if check_y_n==0:
+         print("greate")
+    elif check_y_n==1:
+        print("most welcome👍")
+except:
+    print("not to worry")
---
+++ camera_access.py
@@ -0,0 +1,25 @@
+#read and show video stream , capture image
+#detect face and show bounding box(haarcascade)
+#flatten the largest face image and save in numpy array
+#repeat the above for multiple people to generate training data
+
+import cv2
+import numpy
+
+#init camera
+cap=cv2.VideoCapture(0)
+
+#face dtection
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+while True:
+    ret,frame =cap.read()
+
+    if ret==False:
+        continue
+
+    cv2.imshow("Frame",frame)
+
+    key_pressed=cv2.waitKey(1) & 0xFF
+    if key_pressed == ord("q"):
+        break
